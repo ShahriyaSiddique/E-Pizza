8e50a717cb9210806a5cf9faa957417e90277e41
Shahriya Siddique <ps.shahriya@gmail.com>
2021-01-15 21:46:36 +0600

    added: cart page, login, register

--- server.js
+++ server.js
@@ -10,14 +10,24 @@ const PORT = process.env.PORT || 3020;
 //assets
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
 // config templete engine
 app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views/"));
 app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/cart", (req, res) => {
+  res.render("cutomers/cart");
+});
+app.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+app.get("/register", (req, res) => {
+  res.render("auth/register");
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
